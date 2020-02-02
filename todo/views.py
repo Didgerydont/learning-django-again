fe21b6053ca36ff866cfdb38c4f09a3043c7a6ca
Will Prendergast <prendygast@hotmail.com>
2020-02-02 16:52:08 +0000

    moving to testing

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,8 @@ from .models import Item
 from .forms import ItemForm
 
 # Create your views here.
+
+
 def get_todo_list(request):
     results = Item.objects.all()
     return render(request, "todo_list.html", {'items': results})
@@ -30,5 +32,12 @@ def edit_an_item(request, id):
             return redirect(get_todo_list)
     else:
         form = ItemForm(instance=item)
-        
+
     return render(request, "item_form.html", {'form': form})
+
+
+def toggle_status(request, id):
+    item = get_object_or_404(Item, pk=id)
+    item.done = not item.done
+    item.save()
+    return redirect(get_todo_list)
